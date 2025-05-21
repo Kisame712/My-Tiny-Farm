9ae1c7a7483059f0f66cee693ef24c278318c4c4
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-05-22 00:12:01 +0530

    Added resource management

--- My Tiny Farm/Assets/Scripts/FoodItem.cs
+++ My Tiny Farm/Assets/Scripts/FoodItem.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class FoodItem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    ResourceManager resourceManager;
+
+    public int waterCost;
+    public int compostCost;
+    public int ureaCost;
+    public int seedCost;
+
+    public int cashGeneratedPerCycle;
+    public float timeBetweenCycles;
+    float nextCycleTime;
+
     void Start()
     {
-        
+        resourceManager = FindObjectOfType<ResourceManager>();
+        resourceManager.waterAmount -= waterCost;
+        resourceManager.compostAmount -= compostCost;
+        resourceManager.ureaAmount -= ureaCost;
+        resourceManager.seedAmount -= seedCost;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time > nextCycleTime)
+        {
+            nextCycleTime = Time.time + timeBetweenCycles;
+            resourceManager.cash += cashGeneratedPerCycle;
+        }
     }
 }
---
+++ My Tiny Farm/Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int cash;
+
+    public int waterAmount;
+    public int compostAmount;
+    public int ureaAmount;
+    public int seedAmount;
+
+    public TMP_Text waterText;
+    public TMP_Text compostText;
+    public TMP_Text ureaText;
+    public TMP_Text seedText;
+    public TMP_Text cashText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        waterText.text = waterAmount.ToString();
+        compostText.text = compostAmount.ToString();
+        ureaText.text = ureaAmount.ToString();
+        seedText.text = seedAmount.ToString();
+        cashText.text = cash.ToString();
+
+    }
+}
