458b01d132e25a7b7def50ba4103004b29f75888
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-05-21 15:59:45 +0530

    Added Skybox functionality

---
+++ My Tiny Farm/Assets/Scripts/BuildPosition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPosition : MonoBehaviour
+{
+    public Transform spawnPoint;
+    BuildManager buildManager;
+    
+    void Start()
+    {
+        buildManager = FindObjectOfType<BuildManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if(buildManager.itemToPlace != null)
+        {
+            Instantiate(buildManager.itemToPlace, spawnPoint.position, spawnPoint.rotation);
+            buildManager.itemToPlace = null;
+        }
+    }
+
+
+}
